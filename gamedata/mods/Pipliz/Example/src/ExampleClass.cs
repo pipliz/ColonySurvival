2243ca88191ef9a3218d26d38d890d79117e52e8
pipliz <pipliznl@gmail.com>
2017-07-19 16:01:33 +0200

    Fleshed out example mod, moved texture mapping types.json to server

--- gamedata/mods/Pipliz/Example/src/ExampleClass.cs
+++ gamedata/mods/Pipliz/Example/src/ExampleClass.cs
@@ -1,104 +1,90 @@
-﻿namespace ExampleMod
+﻿using Pipliz;
+using Pipliz.Chatting;
+using Pipliz.JSON;
+using Pipliz.Threading;
+
+namespace ExampleMod
 {
 	[ModLoader.ModManager]
-	public class ExampleClass
+	public class ExampleClassModManager
 	{
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.AfterAssemblyLoad)]
-		public static void AfterAssemblyLoad ()
+		[ModLoader.ModCallback(ModLoader.EModCallbackType.AfterAddingBaseTypes)]
+		public static void AfterAddingBaseTypes ()
 		{
-			Pipliz.Log.Write("Hello from ExampleClass.AfterAssemblyLoad");
+			ItemTypesServer.AddTextureMapping("ExampleBlock1", new JSONNode(NodeType.Object)
+				.SetAs("albedo", "grassTemperate")
+				.SetAs("normal", "stoneblock")
+				.SetAs("emissive", "ovenLitFront")
+				.SetAs("height", "oreCoal")
+			);
+
+			ItemTypesServer.AddTextureMapping("ExampleBlock2", new JSONNode(NodeType.Object)
+				.SetAs("albedo", "grindstone")
+				.SetAs("normal", "berrybush")
+				.SetAs("emissive", "torch")
+				.SetAs("height", "snow")
+			);
+			
+			ItemTypes.AddRawType("ExampleBlock1", 
+				new JSONNode(NodeType.Object)
+					.SetAs("onPlaceAudio", "stonePlace")
+					.SetAs("onRemoveAudio", "woodDeleteLight")
+					.SetAs("sideall", "SELF")
+					.SetAs("onRemoveAmount", 2)
+					.SetAs("maxStackSize", 500)
+					.SetAs("npcLimit", 1000)
+			);
+
+			ItemTypes.AddRawType("ExampleBlock2",
+				new JSONNode(NodeType.Object)
+					.SetAs("onPlaceAudio", "stonePlace")
+					.SetAs("onRemoveAudio", "woodDeleteLight")
+					.SetAs("sideall", "SELF")
+					.SetAs("onRemoveAmount", 2)
+					.SetAs("maxStackSize", 500)
+					.SetAs("npcLimit", 1000)
+			);
+
+		}
+		
+		[ModLoader.ModCallback(ModLoader.EModCallbackType.AfterItemTypesServer)]
+		public static void AfterItemTypesServer ()
+		{
+			ItemTypesServer.RegisterType("ExampleBlock1",
+				new ItemTypesServer.ItemActionBuilder()
+				.SetOnAdd(ExampleClassCodeManager.OnAdd)
+				.SetOnRemove(ExampleClassCodeManager.OnRemove)
+				.SetOnChange(ExampleClassCodeManager.OnChange)
+				.SetChangeTypes("ExampleBlock1", "ExampleBlock2")
+			);
+
+			ItemTypesServer.RegisterParent("ExampleBlock2", "ExampleBlock1");
+		}
+	}
+
+	static class ExampleClassCodeManager
+	{
+		public static void OnAdd (Vector3Int position, ushort newType, NetworkID causedBy)
+		{
+			Chat.Send(causedBy, string.Format("You placed block {0} at {1}", ItemTypes.IndexLookup.GetName(newType), position));
+			ThreadManager.InvokeOnMainThread(delegate ()
+		   {
+			   ServerManager.TrySetBlock(position, ItemTypes.IndexLookup.GetIndex("ExampleBlock2"), causedBy, false);
+		   }, 5.0);
 		}
 
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.AfterItemTypesDefined)]
-		public static void AfterItemTypesDefined ()
+		public static void OnRemove (Vector3Int position, ushort wasType, NetworkID causedBy)
 		{
-			Pipliz.Log.Write("Hello from ExampleClass.AfterItemTypesDefined");
+			Chat.Send(causedBy, string.Format("You removed block {0} at {1}", ItemTypes.IndexLookup.GetName(wasType), position));
 		}
 
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.AfterNetworkSetup)]
-		public static void AfterNetworkSetup ()
+		public static void OnChange (Vector3Int position, ushort wasType, ushort newType, NetworkID causedBy)
 		{
-			Pipliz.Log.Write("Hello from ExampleClass.AfterNetworkSetup");
-		}
-
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.AfterSelectedWorld)]
-		public static void AfterSelectedWorld ()
-		{
-			Pipliz.Log.Write("Hello from ExampleClass.AfterSelectedWorld");
-		}
-
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.AfterStartup)]
-		public static void AfterStartup ()
-		{
-			Pipliz.Log.Write("Hello from ExampleClass.AfterStartup");
-		}
-
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.AfterWorldLoad)]
-		public static void AfterWorldLoad ()
-		{
-			Pipliz.Log.Write("Hello from ExampleClass.AfterWorldLoad");
-		}
-
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.OnApplicationFocus)]
-		public static void OnApplicationFocus (bool state)
-		{
-			Pipliz.Log.Write("Hello from ExampleClass.OnApplicationFocus");
-		}
-
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.OnApplicationPause)]
-		public static void OnApplicationPause (bool state)
-		{
-			Pipliz.Log.Write("Hello from ExampleClass.OnApplicationPause");
-		}
-
-		static int fixedUpdateCount = 0;
-
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.OnFixedUpdate)]
-		public static void OnFixedUpdate ()
-		{
-			if (fixedUpdateCount++ % 200 == 0) {
-				Pipliz.Log.Write("Hello from ExampleClass.OnFixedUpdate");
-			}
-		}
-
-		static int lateUpdateCount = 0;
-
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.OnLateUpdate)]
-		public static void OnLateUpdate ()
-		{
-			if (lateUpdateCount++ % 450 == 0) {
-				Pipliz.Log.Write("Hello from ExampleClass.OnLateUpdate");
-			}
-		}
-
-		static int updateCount = 0;
-
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.OnUpdate)]
-		public static void OnUpdate ()
-		{
-			if (updateCount++ % 500 == 0) {
-				Pipliz.Log.Write("Hello from ExampleClass.OnUpdate");
-			}
-		}
-
-		static int updateEndCount = 0;
-
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.OnUpdateEnd)]
-		public static void OnUpdateEnd ()
-		{
-			if (updateEndCount++ % 500 == 0) {
-				Pipliz.Log.Write("Hello from ExampleClass.OnUpdateEnd");
-			}
-		}
-
-		static int updateStartCount = 0;
-
-		[ModLoader.ModCallback(ModLoader.EModCallbackType.OnUpdateStart)]
-		public static void OnUpdateStart ()
-		{
-			if (updateStartCount++ % 500 == 0) {
-				Pipliz.Log.Write("Hello from ExampleClass.OnUpdateStart");
-			}
+			Chat.SendToAll(string.Format("Block type {0} changed to {1} at {2}",
+				ItemTypes.IndexLookup.GetName(wasType),
+				ItemTypes.IndexLookup.GetName(newType),
+				position)
+			);
 		}
 	}
 }
