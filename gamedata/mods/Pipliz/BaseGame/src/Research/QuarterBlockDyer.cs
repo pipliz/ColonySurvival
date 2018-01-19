7849dc8baa0b48b24615421271abe3bdc447a394
pipliz <pipliznl@gmail.com>
2018-01-19 16:14:18 +0100

    Quarter block recipes/locales/research

---
+++ gamedata/mods/Pipliz/BaseGame/src/Research/QuarterBlockDyer.cs
@@ -0,0 +1,25 @@
+﻿using Pipliz.Mods.APIProvider.Science;
+using Server.Science;
+
+namespace Pipliz.Mods.BaseGame.Researches
+{
+	[AutoLoadedResearchable]
+	public class QuarterBlockDyer : BaseResearchable
+	{
+		public QuarterBlockDyer ()
+		{
+			key = "pipliz.baseresearch.quarterblockdyer";
+			//icon = "gamedata/textures/icons/quarterblock.png";
+			iterationCount = 10;
+			AddIterationRequirement("sciencebagbasic");
+			AddDependency("pipliz.baseresearch.dyertable");
+		}
+
+		public override void OnResearchComplete (ScienceManagerPlayer manager, EResearchCompletionReason reason)
+		{
+			var storage = RecipeStorage.GetPlayerStorage(manager.Player);
+			storage.SetRecipeAvailability("pipliz.dyer.quarterblockblack", true, "pipliz.dyer");
+			storage.SetRecipeAvailability("pipliz.dyer.quarterblockwhite", true, "pipliz.dyer");
+		}
+	}
+}
---
+++ gamedata/mods/Pipliz/BaseGame/src/Research/QuarterBlockSplittingStump.cs
@@ -0,0 +1,25 @@
+﻿using Pipliz.Mods.APIProvider.Science;
+using Server.Science;
+
+namespace Pipliz.Mods.BaseGame.Researches
+{
+	[AutoLoadedResearchable]
+	public class QuarterBlockSplittingStump : BaseResearchable
+	{
+		public QuarterBlockSplittingStump ()
+		{
+			key = "pipliz.baseresearch.quarterblocksplittingstump";
+			//icon = "gamedata/textures/icons/quarterblock.png";
+			iterationCount = 10;
+			AddIterationRequirement("sciencebagbasic");
+			AddDependency("pipliz.baseresearch.splittingstump");
+		}
+
+		public override void OnResearchComplete (ScienceManagerPlayer manager, EResearchCompletionReason reason)
+		{
+			var storage = RecipeStorage.GetPlayerStorage(manager.Player);
+			storage.SetRecipeAvailability("pipliz.dyer.quarterblockbrowndark", true, "pipliz.woodcutter");
+			storage.SetRecipeAvailability("pipliz.dyer.quarterblockbrownlight", true, "pipliz.woodcutter");
+		}
+	}
+}
