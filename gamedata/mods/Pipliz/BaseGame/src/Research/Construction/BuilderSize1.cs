a1ac27aace9dec9ede9e6e74095a43e749744901
pipliz <pipliznl@gmail.com>
2018-03-21 15:23:33 +0100

    Added builder-digger research & size limitations

---
+++ gamedata/mods/Pipliz/BaseGame/src/Research/Construction/BuilderSize1.cs
@@ -0,0 +1,25 @@
+﻿using Pipliz.Mods.APIProvider.Science;
+using Server.Science;
+
+namespace Pipliz.Mods.BaseGame.Researches
+{
+	[AutoLoadedResearchable]
+	public class ConstructionBuilderSize1 : BaseResearchable
+	{
+		public ConstructionBuilderSize1 ()
+		{
+			key = "pipliz.baseresearch.constructionbuildersize1";
+			icon = "gamedata/textures/icons/buildersize1.png";
+			iterationCount = 30;
+			AddIterationRequirement("sciencebagbasic");
+			AddIterationRequirement("stoneblock", 5);
+			AddDependency("pipliz.baseresearch.constructionbuilder");
+		}
+
+		public override void OnResearchComplete (ScienceManagerPlayer manager, EResearchCompletionReason reason)
+		{
+			manager.Player.GetTempValues(true).Set("pipliz.builderlimit", 25000);
+			ConstructionHelper.SendPacket(manager.Player);
+		}
+	}
+}
---
+++ gamedata/mods/Pipliz/BaseGame/src/Research/Construction/DiggerSize5.cs
@@ -0,0 +1,25 @@
+﻿using Pipliz.Mods.APIProvider.Science;
+using Server.Science;
+
+namespace Pipliz.Mods.BaseGame.Researches
+{
+	[AutoLoadedResearchable]
+	public class ConstructionDiggerSize5 : BaseResearchable
+	{
+		public ConstructionDiggerSize5 ()
+		{
+			key = "pipliz.baseresearch.constructiondiggersize5";
+			icon = "gamedata/textures/icons/diggersize5.png";
+			iterationCount = 250;
+			AddIterationRequirement("sciencebagadvanced");
+			AddIterationRequirement("sciencebagcolony");
+			AddDependency("pipliz.baseresearch.constructiondiggersize4");
+		}
+
+		public override void OnResearchComplete (ScienceManagerPlayer manager, EResearchCompletionReason reason)
+		{
+			manager.Player.GetTempValues(true).Set("pipliz.diggerlimit", 1000000);
+			ConstructionHelper.SendPacket(manager.Player);
+		}
+	}
+}
