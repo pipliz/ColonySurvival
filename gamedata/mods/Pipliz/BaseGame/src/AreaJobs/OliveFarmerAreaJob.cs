f6c1a0e47aae4aa7cda27dc6f7cefc231d0b24c1
pipliz <pipliznl@gmail.com>
2019-07-25 07:59:54 +0200

    0.7.0.117

--- gamedata/mods/Pipliz/BaseGame/src/AreaJobs/OliveFarmerAreaJob.cs
+++ gamedata/mods/Pipliz/BaseGame/src/AreaJobs/OliveFarmerAreaJob.cs
@@ -2,6 +2,7 @@
 using Jobs;
 using NPC;
 using Recipes;
+using System.Collections.Generic;
 
 namespace Pipliz.Mods.BaseGame.AreaJobs
 {
@@ -31,7 +32,11 @@ namespace Pipliz.Mods.BaseGame.AreaJobs
 		{
 			// store treeLocation separately from positionSub because the farmer will move next to these positions(they're not equal)
 			protected Vector3Int treeLocation = Vector3Int.invalidPos;
-			static ItemTypes.ItemType[] yTypesBuffer = new ItemTypes.ItemType[5]; // max 3 Y + 1 below + 1 above
+
+			static readonly ItemTypes.ItemType[] yTypesBuffer = new ItemTypes.ItemType[5]; // max 3 Y + 1 below + 1 above
+			static readonly List<Vector3Int> treePositions = new List<Vector3Int>();
+			static readonly List<RecipeResult> GatherResults = new List<RecipeResult>();
+
 			public int GatheredCount { get; set; }
 
 			public OliveFarmerJob (AbstractAreaJobDefinition def, Colony owner, Vector3Int min, Vector3Int max, int npcID = 0) : base(def, owner, min, max, npcID) { }
@@ -43,46 +48,19 @@ namespace Pipliz.Mods.BaseGame.AreaJobs
 				Vector3Int max = Maximum;
 				int ySize = max.y - min.y + 1;
 
+				treePositions.Clear();
 				for (int x = min.x + 1; x < max.x; x += 3) {
 					for (int z = min.z + 1; z < max.z; z += 3) {
-						if (!World.TryGetColumn(new Vector3Int(x, min.y - 1, z), ySize + 2, yTypesBuffer)) {
-							goto DUMB_RANDOM;
-						}
-
-						for (int y = 0; y < ySize; y++) {
-							ItemTypes.ItemType typeBelow = yTypesBuffer[y];
-							ItemTypes.ItemType type = yTypesBuffer[y + 1];
-							ItemTypes.ItemType typeAbove = yTypesBuffer[y + 2];
-
-							if (
-								(type == BuiltinBlocks.Types.air || type == BuiltinBlocks.Types.logtemperate)
-								&& (typeAbove == BuiltinBlocks.Types.air || typeAbove == BuiltinBlocks.Types.logtemperate)
-								&& typeBelow.IsFertile
-							) {
-								treeLocation = new Vector3Int(x, min.y + y, z);
-								bool canStand;
-								Vector3Int position;
-								if (AI.PathingManager.TryGetClosestPositionWorldNotAt(treeLocation, NPC.Position, out canStand, out position)) {
-									if (canStand) {
-										positionSub = position;
-									} else {
-										positionSub = Vector3Int.invalidPos;
-									}
-								} else {
-									positionSub = Vector3Int.invalidPos;
-								}
-								return;
-							}
-						}
+						treePositions.Add(new Vector3Int(x, 0, z));
 					}
 				}
 
-				for (int i = 0; i < 15; i++) {
-					// give the random positioning 5 chances to become valid
-					int testX = min.x + Random.Next(0, (max.x - min.x - 2) / 3 + 1) * 3;
-					int testZ = min.z + Random.Next(0, (max.z - min.z - 2) / 3 + 1) * 3;
+				while (treePositions.Count > 0) {
+					int idx = Random.Next(0, treePositions.Count);
+					Vector3Int pos = treePositions[idx];
+					treePositions.RemoveAt(idx);
 
-					if (!World.TryGetColumn(new Vector3Int(testX, min.y - 1, testZ), ySize + 2, yTypesBuffer)) {
+					if (!World.TryGetColumn(new Vector3Int(pos.x, min.y - 1, pos.z), ySize + 2, yTypesBuffer)) {
 						goto DUMB_RANDOM;
 					}
 
@@ -91,13 +69,23 @@ namespace Pipliz.Mods.BaseGame.AreaJobs
 						ItemTypes.ItemType type = yTypesBuffer[y + 1];
 						ItemTypes.ItemType typeAbove = yTypesBuffer[y + 2];
 
-						if (typeBelow.BlocksPathing && !type.BlocksPathing && !typeAbove.BlocksPathing) {
-							positionSub = new Vector3Int(testX, min.y + y, testZ);
-							treeLocation = Vector3Int.invalidPos;
-							return;
+						if (
+							(type == BuiltinBlocks.Types.air || type == BuiltinBlocks.Types.logtemperate || type == BuiltinBlocks.Types.olivesapling)
+							&& (typeAbove == BuiltinBlocks.Types.air || typeAbove == BuiltinBlocks.Types.logtemperate)
+							&& typeBelow.IsFertile
+						) {
+							treeLocation = new Vector3Int(pos.x, min.y + y, pos.z);
+							if (AI.PathingManager.TryGetClosestPositionWorldNotAt(treeLocation, NPC.Position, out bool canStand, out Vector3Int position)) {
+								if (canStand) {
+									positionSub = position;
+									return;
+								}
+							}
 						}
 					}
 				}
+
+				// none of the spots were viable tree locations (???)
 
 				DUMB_RANDOM:
 				treeLocation = Vector3Int.invalidPos;
@@ -108,8 +96,6 @@ namespace Pipliz.Mods.BaseGame.AreaJobs
 				);
 			}
 
-			static System.Collections.Generic.List<RecipeResult> GatherResults = new System.Collections.Generic.List<RecipeResult>();
-
 			public override void OnNPCAtJob (ref NPCBase.NPCState state)
 			{
 				OliveFarmerAreaJob def = (OliveFarmerAreaJob)definition;
@@ -117,20 +103,17 @@ namespace Pipliz.Mods.BaseGame.AreaJobs
 				ThreadManager.AssertIsMainThread();
 				state.JobIsDone = true;
 				positionSub = Vector3Int.invalidPos;
-				Vector3Int min = Minimum;
-				Vector3Int max = Maximum;
 				if (!treeLocation.IsValid) { // probably idling about
 					state.SetCooldown(Random.NextFloat(8f, 16f));
 					return;
 				}
 
-				ushort type;
-				if (!World.TryGetTypeAt(treeLocation, out type)) {
+				if (!World.TryGetTypeAt(treeLocation, out ushort type)) {
 					state.SetCooldown(10.0);
 					return;
 				}
 
-				if (type == BuiltinBlocks.Indices.logtemperate) {
+				if (type == BuiltinBlocks.Indices.logtemperate) { // gather olives
 					var recipes = Owner.RecipeData.GetAvailableRecipes(def.NPCTypeString);
 					Recipe.RecipeMatch match = Recipe.MatchRecipe(recipes, Owner);
 					switch (match.MatchType) {
@@ -175,8 +158,7 @@ namespace Pipliz.Mods.BaseGame.AreaJobs
 							}
 							return;
 					}
-				} else if (type == 0) {
-					// maybe plant sapling?
+				} else if (type == 0) { // maybe plant sapling?
 					if (World.TryGetTypeAt(treeLocation.Add(0, -1, 0), out ItemTypes.ItemType typeBelow)) {
 						if (typeBelow.IsFertile) {
 							ServerManager.TryChangeBlock(treeLocation, BuiltinBlocks.Types.air, BuiltinBlocks.Types.olivesapling, Owner, ESetBlockFlags.DefaultAudio);
@@ -187,10 +169,12 @@ namespace Pipliz.Mods.BaseGame.AreaJobs
 						state.SetCooldown(10.0);
 						return;
 					}
+				} else {
+					// very likely it's a sapling, so idle about
 				}
 
-				// something unexpected
-				state.SetCooldown(Random.NextFloat(8f, 16f));
+				// something unexpected or idling
+				state.SetCooldown(Random.NextFloat(4f, 8f));
 				return;
 			}
 		}
